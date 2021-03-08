0475a3370683937a68201b8178b4f9325d66d7aa
Karoline Dias Barreto <karol.db1230@gmail.com>
2021-03-08 11:22:16 -0300

    Atualização contexto ChallengesContext

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import challenges from '../../challenges.json';
 import { IChallengesContextData } from '../interfaces/IChallengesContextData';
 import { IChallengesProviderProps } from '../interfaces/IChallengesProviderProps';
@@ -12,22 +12,48 @@ export function ChallengesProvider({children} : IChallengesProviderProps){
     const [activeChallenge, setActiveChallenge] = useState(null);
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
+
     function levelUp(){
         setLevel(level + 1);
     }
 
     function startNewChallenge(){
-        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        // console.log(Notification.permission);
+        if (Notification.permission == 'granted'){
+            new Notification('Novo desafio', {
+                body: `Valendo ${challenge.amount}xp`
+            })
+        }
     }
 
     function resetChallenge(){
         setActiveChallenge(null);
     }
 
-    function updateXp(){
-        setCurrentExperience(currentExperience + activeChallenge.amount);
+    function completeChallenge(){
+        if(!activeChallenge){
+            return;
+        }
+
+        const { amount} = activeChallenge;
+        let finalExperiente = currentExperience + amount;
+
+        if(finalExperiente >= experienceToNextLevel){
+            finalExperiente = finalExperiente - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperiente);
+        setActiveChallenge(null);
         setChallengesCompleted(challengesCompleted + 1);
     }
 
@@ -42,7 +68,7 @@ export function ChallengesProvider({children} : IChallengesProviderProps){
                 startNewChallenge,
                 activeChallenge,
                 resetChallenge,
-                updateXp
+                completeChallenge
             }}
         >
             {children}
